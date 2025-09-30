35a9f7e77f88fb0889f1965961eb857bc340b574
Mariohood <manhattan199@hotmail.com>
2025-09-30 18:46:18 -0300

    30 set Mostrando usuários Btn Editar Btn Eliminar

--- src/views/admin/AdminView.vue
+++ src/views/admin/AdminView.vue
@@ -10,13 +10,22 @@ const { usuariosCollection } = useUsuarios();
     Novo Usuário
   </v-btn>
 
-  <v-card>
+  <v-card class="mx-auto mt-10" flat>
     <v-list>
       <v-list-item v-for="usuario in usuariosCollection" :key="usuario.id">
+        <template v-slot:prepend>
+          <v-list-item-media start="true">
+            <img width="130" :src="usuario.image" />
+          </v-list-item-media>
+        </template>
         <v-list-item-title
           >{{ usuario.nome }} {{ usuario.sobrenome }}</v-list-item-title
         >
         <v-list-item-subtitle>{{ usuario.curso }}</v-list-item-subtitle>
+        <template v-slot:append>
+          <v-btn color="info" flat class="mr-2"> Editar </v-btn>
+          <v-btn color="red-darken-3" flat> Eliminar </v-btn>
+        </template>
       </v-list-item>
     </v-list>
   </v-card>

7575715bb02514eca8c34d8bd98b057f590a9d10
Mariohood <manhattan199@hotmail.com>
2025-10-09 18:54:18 -0300

    09oct  Consulta db e mostra usuários como card

---
+++ src/components/Usuario.vue
@@ -0,0 +1,6 @@
+<script setup></script>
+<template>
+  <v-col cols="12" md="4">
+    <h2>Usuario...</h2>
+  </v-col>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,20 @@
 <script setup>
+import useUsuarios from "../composables/useUsuarios";
+import Usuario from "../components/Usuario.vue";
 
+const { usuariosCollection } = useUsuarios();
 </script>
 
 <template>
-  <h1 class="text-center text-h3 font-weight-bold my-5">IFSeg</h1>
+  <h1 class="text-center text-h3 font-weight-bold my-5">Nossos usuários</h1>
+
+  <v-card flat class="py-10">
+    <v-row>
+      <usuario
+        v-for="Usuario in usuariosCollection"
+        :key="usuariosCollection.id"
+        :usuario="usuario"
+      />
+    </v-row>
+  </v-card>
 </template>

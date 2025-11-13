3afc89aa6eb0fd5936f3e9e778ad37ac0f5d267b
Mariohood <manhattan199@hotmail.com>
2025-11-13 19:05:31 -0300

    10Nov mostrando a informçãodo usuário na sua paguina

--- src/views/UsuarioView.vue
+++ src/views/UsuarioView.vue
@@ -1,13 +1,27 @@
 <script setup>
 import { useRoute } from "vue-router";
+import { doc } from "firebase/firestore";
+import { useDocument, useFirestore } from "vuefire";
 
 const route = useRoute();
-
-console.log(route.params.id);
+const db = useFirestore();
+const docRef = doc(db, "usuarios", route.params.id);
+const usuario = useDocument(docRef);
 </script>
 
 <template>
   <div>
-    <h1>Usuario....</h1>
+    <v-card flat>
+      <v-card-title class="mt-5 text-h3 text-center py-5 font-weight-bold">
+        {{ usuario?.nome }} {{ usuario?.sobrenome }}
+      </v-card-title>
+      <v-img
+        :src="usuario?.image"
+        width="250"
+        height="400"
+        class="mx-auto"
+        cover
+      />
+    </v-card>
   </div>
 </template>
